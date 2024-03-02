f8c255e2541025848a79b60a3377b52a0b48e2cd
Aishwary Vishwakarma <aishwaryvishwakarma23@gmail.com>
2024-03-02 11:54:31 +0530

    refactor: home page caarousel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,84 @@
 import Carousel from '@/components/Carousel/Carousel';
 import Layout from '@/components/Layout/Layout';
+import {nanoid} from '@reduxjs/toolkit';
+import Image from 'next/image';
 
+import Gel from '../../public/gel.jpg';
 import styles from './styles.module.scss';
+
+const slides = [
+  <li key={nanoid()} className={styles.slide}>
+    <div className={styles.videoContainer}>
+      <video autoPlay muted loop playsInline controls={false}>
+        <source src='/cup.webm' type='video/webm' />
+        <source src='/cup.mp4' type='video/mp4' />
+      </video>
+      <div className={styles.backdrop} />
+      <div className={styles.content}>
+        <span>Magic Cups</span>
+        <br /> Sip & Be Amazed!
+      </div>
+    </div>
+    <div className={`${styles.videoContainer} ${styles.sewing}`}>
+      <video autoPlay muted loop playsInline controls={false}>
+        <source src='/sewing.webm' type='video/webm' />
+        <source src='/sewing.mp4' type='video/mp4' />
+      </video>
+      <div className={styles.backdrop} />
+      <div className={styles.content}>
+        <span>Revolutionize Your Craft</span> <br /> Sewing Machines Unleashed!
+      </div>
+    </div>
+    <div className={`${styles.imageContainer} ${styles.gel}`}>
+      <Image
+        src={Gel.src}
+        alt='Body refreshing gel'
+        height={Gel.height}
+        width={Gel.width}
+        placeholder='blur'
+        blurDataURL={Gel.blurDataURL}
+        loading='lazy'
+      />
+      <div className={styles.backdrop} />
+      <div className={styles.content}>
+        <span>Revitalize Your Skin</span> <br /> Body Refreshing Gel!
+      </div>
+    </div>
+  </li>,
+  <li key={nanoid()} className={styles.slide}>
+    <div className={styles.videoContainer}>
+      <video autoPlay muted loop playsInline controls={false}>
+        <source src='/cup.mp4' type='video/mp4' />
+      </video>
+      <div className={styles.backdrop} />
+      <div className={styles.content}>
+        <span>Magic Cups</span>
+        <br /> Sip & Be Amazed!
+      </div>
+    </div>
+  </li>,
+  <li key={nanoid()} className={styles.slide}>
+    <div className={styles.videoContainer}>
+      <video autoPlay muted loop playsInline controls={false}>
+        <source src='/cup.mp4' type='video/mp4' />
+      </video>
+      <div className={styles.backdrop} />
+      <div className={styles.content}>
+        <span>Magic Cups</span>
+        <br /> Sip & Be Amazed!
+      </div>
+    </div>
+    <div className={`${styles.videoContainer} ${styles.sewing}`}>
+      <video autoPlay muted loop playsInline controls={false}>
+        <source src='/sewing.mp4' type='video/mp4' />
+      </video>
+      <div className={styles.backdrop} />
+      <div className={styles.content}>
+        <span>Revolutionize Your Craft</span> <br /> Sewing Machines Unleashed!
+      </div>
+    </div>
+  </li>,
+];
 
 const HomePage = () => {
   return (
@@ -22,7 +99,7 @@ const HomePage = () => {
         </div>
       </header>
       <section className={`${styles.bestseller} layouted`}>
-        <Carousel heading='Bestseller' />
+        <Carousel heading='Bestseller' slidesData={slides} />
       </section>
     </Layout>
   );
--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -1,28 +1,24 @@
 'use client';
 
 import {Caret} from '@/assets/icons';
-import Image from 'next/image';
 import React, {type HTMLAttributes} from 'react';
 
-import Gel from '../../../public/gel.jpg';
 import styles from './styles.module.scss';
-
-// NOTE: This compnent can become moree simplified and reusable by passing the whole slides
-// as a content thrugh props and rendering them, but for the time being I'm hardcoding the data
 
 interface Props extends HTMLAttributes<HTMLDivElement> {
   heading: string;
+  slidesData: JSX.Element[];
 }
 
 const Carousel: React.FC<Props> = (data) => {
-  const {heading, className, ...rest} = data ?? {};
+  const {heading, slidesData = [], className, ...rest} = data ?? {};
 
   const slidesRef = React.useRef<HTMLUListElement | null>(null);
 
   const [currentIdx, setCurrentIdx] = React.useState(0);
 
   const next = (): void => {
-    if (currentIdx >= 2) return;
+    if (currentIdx >= slidesData.length - 1) return;
 
     setCurrentIdx((prev): typeof prev => prev + 1);
   };
@@ -52,86 +48,14 @@ const Carousel: React.FC<Props> = (data) => {
             className={currentIdx <= 0 ? styles.disabled : ''}
           />
           <Caret
-            className={`${styles.rightIcon} ${currentIdx >= 2 && styles.disabled}`}
+            className={`${styles.rightIcon} ${currentIdx >= slidesData.length - 1 && styles.disabled}`}
             onClick={next}
           />
         </div>
       </div>
       <div className={styles.slidesContainer}>
         <ul ref={slidesRef} className={styles.slides}>
-          <li className={styles.slide} data-item='3'>
-            <div className={styles.videoContainer}>
-              <video autoPlay muted loop playsInline controls={false}>
-                <source src='/cup.webm' type='video/webm' />
-                <source src='/cup.mp4' type='video/mp4' />
-              </video>
-              <div className={styles.backdrop} />
-              <div className={styles.content}>
-                <span>Magic Cups</span>
-                <br /> Sip & Be Amazed!
-              </div>
-            </div>
-            <div className={`${styles.videoContainer} ${styles.sewing}`}>
-              <video autoPlay muted loop playsInline controls={false}>
-                <source src='/sewing.webm' type='video/webm' />
-                <source src='/sewing.mp4' type='video/mp4' />
-              </video>
-              <div className={styles.backdrop} />
-              <div className={styles.content}>
-                <span>Revolutionize Your Craft</span> <br /> Sewing Machines
-                Unleashed!
-              </div>
-            </div>
-            <div className={`${styles.imageContainer} ${styles.gel}`}>
-              <Image
-                src={Gel.src}
-                alt='Body refreshing gel'
-                height={Gel.height}
-                width={Gel.width}
-                placeholder='blur'
-                blurDataURL={Gel.blurDataURL}
-                loading='lazy'
-              />
-              <div className={styles.backdrop} />
-              <div className={styles.content}>
-                <span>Revitalize Your Skin</span> <br /> Body Refreshing Gel!
-              </div>
-            </div>
-          </li>
-          <li className={styles.slide} data-item='1'>
-            <div className={styles.videoContainer}>
-              <video autoPlay muted loop playsInline controls={false}>
-                <source src='/cup.mp4' type='video/mp4' />
-              </video>
-              <div className={styles.backdrop} />
-              <div className={styles.content}>
-                <span>Magic Cups</span>
-                <br /> Sip & Be Amazed!
-              </div>
-            </div>
-          </li>
-          <li className={styles.slide} data-item='2'>
-            <div className={styles.videoContainer}>
-              <video autoPlay muted loop playsInline controls={false}>
-                <source src='/cup.mp4' type='video/mp4' />
-              </video>
-              <div className={styles.backdrop} />
-              <div className={styles.content}>
-                <span>Magic Cups</span>
-                <br /> Sip & Be Amazed!
-              </div>
-            </div>
-            <div className={`${styles.videoContainer} ${styles.sewing}`}>
-              <video autoPlay muted loop playsInline controls={false}>
-                <source src='/sewing.mp4' type='video/mp4' />
-              </video>
-              <div className={styles.backdrop} />
-              <div className={styles.content}>
-                <span>Revolutionize Your Craft</span> <br /> Sewing Machines
-                Unleashed!
-              </div>
-            </div>
-          </li>
+          {slidesData.map((slide) => slide)}
         </ul>
       </div>
     </div>
